035c7632c64837ee0d583dc98900552cfc9a03f7
Vladimir Kalpakchiew <vkalpakchiew@gmail.com>
2019-04-19 00:44:51 +0300

    New files has been added to the project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Centuries = ");
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            int minutes = hours * 60;
+            Console.WriteLine("{0} Centuries = {1} Years = {2} Days = {3} Hours = {4} Minutes", centuries, years, days, hours, minutes);
+        }
+    }
+}
